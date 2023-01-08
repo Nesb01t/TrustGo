19820f7522cec9c326d59ecf053219e724a55afe
nesb01t <nesb01t@hotmail.com>
2023-01-08 13:45:44 +0800

    Update init pages

---
+++ pages/main.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		Hello world
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
