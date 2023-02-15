33d1c868c56498c70446963b6d2d3f633cb5022f
nesb01t <littlelumos@foxmail.com>
2023-02-15 08:10:50 +0800

    Update fix some problem

--- pages/main.vue
+++ pages/main.vue
@@ -3,48 +3,37 @@
 		<Evaluate v-if="currentPage=='评估'"></Evaluate>
 		<Discuss v-if="currentPage=='社区'"></Discuss>
 		<Personal v-if="currentPage=='我的'"></Personal>
-		<u-tabbar
-			:value="value1"
-			:placeholder="false"
-			@change="name => value1 = name"
-			:fixed="true"
-			:safeAreaInsetBottom="false"
-			
-		>
-		    <u-tabbar-item text="评估" icon="search" v-for="(item, index) in pageGroup" :key="index" @click="click1(item.text)" ></u-tabbar-item>
-			<u-tabbar-item text="社区" icon="photo"  @click="click1(item.text)" ></u-tabbar-item>
-			<u-tabbar-item text="我的" icon="account" @click="click1(item.text)" ></u-tabbar-item>
+		<u-tabbar :value='currentPage' :placeholder="false" :fixed="true" :safeAreaInsetBottom="false">
+			<u-tabbar-item :text="item.text" :name="item.text" icon="search" v-for="(item, index) in pageGroup" :key="index"
+				@click="handleSelect(item.text)"></u-tabbar-item>
 		</u-tabbar>
 	</view>
-
 </template>
 
 <script>
-	import Evaluate from './evaluate/evaluate.vue'	
+	import Evaluate from './evaluate/evaluate.vue'
 	import Discuss from './discuss/discuss.vue'
 	import Personal from './personal/personal.vue'
 	export default {
 		data() {
 			return {
-				currentPage: '评估',
+				currentPage: "评估",
 				pageGroup: [{
-					    text: "评估"
+						text: "评估",
 					},
 					{
-						text: "社区"
+						text: "社区",
 					},
 					{
-						text: "我的"
+						text: "我的",
 					}
-					
 				],
-				value1: 1,
 			}
 		},
 		methods: {
-			click1(itemLabel) {
-				console.log('click1', itemLabel);
-				this.currentPage = itemLabel
+			handleSelect(page) {
+				console.log('click: ', page);
+				this.currentPage = page
 			}
 		},
 		components: {
@@ -52,9 +41,8 @@
 			Discuss,
 			Personal
 		},
-		
+
 	}
-	
 </script>
 
 <style lang="scss">
